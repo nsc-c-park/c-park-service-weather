cef744875d25837bfb2374eaac7b5af8219f7529
Krzysztof Samson <krzysztof.samson@capgemini.com>
2017-04-10 08:30:05 +0200

    Another try to start

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const cleanup = require('gulp-cleanup-dest');
-var nodemon = require('nodemon');
-var livereload = require('gulp-livereload');
 
 const tsc = ts.createProject('tsconfig.json');
 
@@ -17,13 +15,4 @@ gulp.task('watch', ['scripts'], () => {
     gulp.watch('src/**/*.ts', ['scripts']);
 });
 
-gulp.task('develop', function() { 
-  livereload.listen();
-  nodemon({script: './dist/index', ext: 'js', legacyWatch: true}).on('restart', function () {
-        setTimeout(function () {
-            livereload.changed();
-        }, 2000); 
-    });
-});
-
-gulp.task('default', ['scripts', 'watch', 'develop']);
+gulp.task('default', ['watch']);
