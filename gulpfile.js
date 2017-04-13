cc0328526c75516fff9742072f80f8b8ea695c21
Samson <krzysztof.samson@capgemini.com>
2017-04-13 12:55:17 +0100

    gulp cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const cleanup = require('gulp-cleanup-dest');
 
 // pull in the project TypeScript config
 const tsProject = ts.createProject('src/tsconfig.json');
 
 gulp.task('scripts', () => {
   const tsResult = tsProject.src()
+  .pipe(cleanup({ dest: 'dist', ext: '.js' }))
   .pipe(tsProject());
   return tsResult.js.pipe(gulp.dest('dist'));
 });
